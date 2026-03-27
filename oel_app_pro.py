509f2c84a37546d7aab0c83eb40ebce5420b1a5b
komparse26-dev <komparse26@gmail.com>
2026-03-27 23:25:11 +0100

    Update oel_app_pro.py

--- oel_app_pro.py
+++ oel_app_pro.py
@@ -6,7 +6,7 @@ from streamlit_autorefresh import st_autorefresh
 
 # --- EINSTELLUNGEN ---
 # Ersetze 'DEIN_API_KEY' mit dem Key von twelvedata.com
-API_KEY = '6c608766b8554cec9.2e0b9aff8da9a6b' 
+API_KEY = '6c608766b8554cec92e0b9aff8da9a6b' 
 SYMBOL = "BRENT" # Symbol für Brent Öl bei Twelve Data
 INTERVALL = "1min" # Zeitinterval (1min, 5min, 15min, 4h, etc.)
 
